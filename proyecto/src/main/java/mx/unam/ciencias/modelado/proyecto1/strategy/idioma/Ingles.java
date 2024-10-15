d6d8ba005a7a53aa27247f0769b587c170cd3b7b
goldenemilly <adamleon@ciencias.unam.mx>
2024-10-15 10:26:23 -0600

    Idioma ha sido comentado e implementado

---
+++ proyecto/src/main/java/mx/unam/ciencias/modelado/proyecto1/strategy/idioma/Ingles.java
@@ -0,0 +1,51 @@
+package mx.unam.ciencias.modelado.proyecto1.strategy.idioma;
+
+/** Clase que implementa el idioma en ingles. 
+* 
+*/
+
+public class Ingles implements Idioma {
+
+    /**
+     * Muestra el catalogo de productos en ingles.
+     * @return Una cadena con la frase "Product catalog:".
+     */
+
+    @Override
+    public String menuCatalogo() {
+        return "Product catalog:";
+    }
+
+    /**
+     * Muestra un saludo en ingles que se muestra despues de iniciar sesion.
+     * @return Una cadena con la frase "Welcome!".
+     */
+
+    @Override
+    public String saludo() {
+        return "Welcome!";
+    }
+
+    /**
+     * Muestra el contenido del ticket en ingles, dando los dias de espera antes de llegaday el contenido del carrito de compra.
+     * @param dias El numero de dias que tardaran en llegar los productos.
+     * @param compra El carrito de compras con los productos adquiridos.
+     * @return Una cadena con los detalles del ticket, incluyendo dias de garantia y productos en el carrito.
+     */
+
+    @Override
+    public String ticket(int dias, Carrito compra) {
+        return "Items in your ticket:\n" +compra+ "\nArrival time: " + dias;
+    }
+
+    /**
+     * Muestra un mensaje de despedida en ingles que se muestra despues de cerrar sesion.
+     * @return Una cadena con la frase "Thank you for your visit, see you soon!".
+     */
+
+    @Override
+    public String despedida() {
+        return "Thank you for your visit, see you soon!";
+    }
+    
+}
